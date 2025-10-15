1e6a58640c6ee68b2f361351296e28cb00e3977d
Jhxnnat <jhonnaromaniteran@gmail.com>
2025-10-14 22:30:11 -0500

    Slow threads

--- threads/threads.c
+++ threads/threads.c
@@ -2,28 +2,51 @@
 #include <pthread.h>
 #include <unistd.h>
 
+#define VALUE 9999999
+
 void normalfunc(int* value) {
-	for(int i=0; i<3; ++i){
-		sleep(2);
+	for(int i=0; i<VALUE; ++i){
 		(*value)++;
-		printf("[%d] Normal function\n", i);
 	}
+	printf("Normal function - value: %d\n", *value);
 }
 
 void* threadfunc(void* arg) {
 	int *ipt = (int *)arg;
-	for(int i=0; i<7; ++i){
-		sleep(1);
-		printf("[%d] > Thread function - value: %d\n", i, *ipt);
+	for(int i=0; i<VALUE; ++i){
+		(*ipt)++;
 	}
+	printf("Thread function - value: %d\n", *ipt);
 	return NULL;
 }
 
-int main(void) {
+void thread_case() {
 	int value = 0;
 	pthread_t new_thread;
 	pthread_create(&new_thread, NULL, threadfunc, &value);
 	normalfunc(&value);
 	pthread_join(new_thread, NULL);
-	return 0;
+}
+
+void no_thread_case() {
+	int value = 0;
+	normalfunc(&value);
+	normalfunc(&value);
+}
+
+int main(void) {
+	printf(" == no_thread_case ==\n");
+	clock_t start = clock();
+	no_thread_case();
+	clock_t end = clock();
+	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+	printf(" == sec: %f", seconds);
+
+	// different result; slower
+	printf("\n\n == thread_case ==\n");
+	start = clock();
+	thread_case();
+	end = clock();
+	seconds = (float)(end - start) / CLOCKS_PER_SEC;
+	printf(" == sec: %f", seconds);
 }
